0cfee779edcbc04cf5d9c45e73cef32054b6ec45
truyen duy <duytruyen.msc@gmail.com>
2022-05-18 17:54:25 +0700

    a

---
+++ src/app/app.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  genders = ['male', 'female'];
+  signupForm:FormGroup;
+  ngOnInit(){
+   this.signupForm=new FormGroup(
+     {
+       'username':new FormControl(null),
+       'email':new FormControl(null),
+       'gender':new FormControl('male'),
+     }
+   )
+  
+  }
+  onSubmit(){
+    console.log(this.signupForm);
+    
+  }
+  
+}
